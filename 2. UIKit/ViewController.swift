5bb289d60d9478d7ee160e0c06294bca21585516
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2022-09-19 15:46:21 +0300

    Initial Commit

---
+++ 2. UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2. UIKit
+//
+//  Created by Дмитрий on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
